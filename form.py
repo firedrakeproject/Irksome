8e1e9e26c361dfa46f9da42e04f8403e893a898a
Rob Kirby <robert.c.kirby@gmail.com>
2019-11-07 13:45:26 -0600

    give the RHS for stage var problem as well

--- form.py
+++ form.py
@@ -14,7 +14,7 @@ class ArgumentReplacer(MultiFunction):
     def argument(self, o):
         return self.arg_map[o]
 
-def getForm(a, butcher, dt):
+def getForm(a, butcher, dt, un):
     # a is the bilinear form for the elliptic part of the operator
     # (i.e. no time derivative)
     # butcher is the A matrix from the Butcher tableau
@@ -27,6 +27,8 @@ def getForm(a, butcher, dt):
     # to set up the right-hand side and take the weighted combination
     # of the stages.  We can do that later.
 
+    # un is a Function holding data for the current time step value
+    
     test, trial = a.arguments()
     assert test.function_space() == trial.function_space()
     fs = test.function_space()
@@ -48,7 +50,14 @@ def getForm(a, butcher, dt):
                                        trial: unew[j]})
             anew += dt * Constant(butcher[i, j]) * map_integrands.map_integrand_dags(mapper, a)
 
-    return Vfs, anew
+    aaction = action(a, un)
+    L = 0
+    for i in range(num_stages):
+        mapper = ArgumentReplacer({test: vnew[i]})
+        L -= map_integrands.map_integrand_dags(mapper, aaction)
+    
+            
+    return Vfs, anew, L
 
 
 AGaussLeg4 = np.array([[0.25, 0.25 - np.sqrt(3) / 6],
@@ -65,7 +74,7 @@ AGaussLeg6 = np.array([[5./36, 2/9 - 1./np.sqrt(15), 5./36 - np.sqrt(15)/30],
 if __name__ == "__main__":
     N = 16
     msh = UnitSquareMesh(N, N)
-    hierarchy = MeshHierarchy(msh, 4)
+    hierarchy = MeshHierarchy(msh, 1)
     msh = hierarchy[-1]
     V = FunctionSpace(msh, "CG", 1)
     u = TrialFunction(V)
@@ -73,33 +82,40 @@ if __name__ == "__main__":
     # good Helmholtz so we can use Neumann BC for now
     a = inner(grad(u), grad(v))*dx + inner(u, v)*dx
 
+    x, y = SpatialCoordinate(msh)
+    u0 = project(sin(pi*x)*sin(pi*y), V)
+    u1 = Function(V)
+    
     dt = Constant(0.1)
 
-    Vfs, anew = getForm(a, Radau35, dt)
-
-    
-    F = Function(Vfs)
-    L = inner(F, anew.arguments()[0])*dx
-
-    uu = Function(Vfs)
-    with uu.dat.vec_wo as x:
-        x.setRandom()
+    Vfs, anew, Lnew = getForm(a, Radau35, dt, u0)
 
     params = {"mat_type": "aij",
-              "ksp_monitor": None,
-              "ksp_type": "gmres",
-              "pc_type": "mg",
-              "mg_levels": {
-                  "ksp_type": "richardson",
-                  "ksp_richardson_scale": 3/4,
-                  "ksp_max_it": 2,
-                  "ksp_monitor_true_residual": None,
-                  "ksp_norm_type": "unpreconditioned",
-                  "pc_type": "pbjacobi"}
-    }
+              "ksp_type": "preonly",
+              "pc_type": "lu"}
+    # params = {"mat_type": "aij",
+    #           "ksp_monitor": None,
+    #           "ksp_type": "gmres",
+    #           "pc_type": "mg",
+    #           "mg_levels": {
+    #               "ksp_type": "richardson",
+    #               "ksp_richardson_scale": 3/4,
+    #               "ksp_max_it": 2,
+    #               "ksp_monitor_true_residual": None,
+    #               "ksp_norm_type": "unpreconditioned",
+    #               "pc_type": "pbjacobi"}
+    # }
 
-    solve(anew==L, uu, solver_parameters=params)
+    k = Function(Vfs)
+    
+    solve(anew==Lnew, k, solver_parameters=params)
 
+    u1.dat.data[:] = u0.dat.data[:]
+    print(u1.dat.data.shape)
+    print(k.dat.data.shape)
+    # hack: u1.dat.data[:] = u0.dat.data[:]
+    # u1.dat.data[:] += dt * k.dat.data @ b from Butcher table
+    # loop over stages, add dt times k[i] into u1
 
 
 
