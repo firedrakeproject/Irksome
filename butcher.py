fa1c4926052e4dd9036af8426ae7928de9a61659
Rob Kirby <robert.c.kirby@gmail.com>
2020-01-14 14:41:44 -0600

    Add some general-order IRK methods and a simple Butcher tableau interface

---
+++ butcher.py
@@ -0,0 +1,65 @@
+import FIAT
+import numpy
+
+
+class ButcherTableau(object):
+    def __init__(self, A, b, c):
+        self.A = A
+        self.b = b
+        self.c = c
+
+
+class BackwardEuler(ButcherTableau):
+    def __init__(self):
+        A = numpy.array([[1.0]])
+        b = numpy.array([1.0])
+        c = numpy.array([1.0])
+        super(BackwardEuler, self).__init__(A, b, c)
+
+
+class CollocationButcherTableau(ButcherTableau):
+    def __init__(self, L):
+        assert L.ref_el == FIAT.ufc_simplex(1)
+
+        for ell in L.dual.nodes:
+            assert isinstance(ell, FIAT.functional.PointEvaluation)
+
+        c = numpy.asarray([list(ell.pt_dict.keys())[0][0]
+                           for ell in L.dual.nodes])
+
+        num_stages = len(c)
+
+        Q = FIAT.make_quadrature(L.ref_el, 2*num_stages)
+        qpts = Q.get_points()
+        qwts = Q.get_weights()
+
+        Lvals = L.tabulate(0, qpts)[0, ]
+
+        # integrates them all!
+        b = Lvals @ qwts
+
+        # now for A, which means we have to adjust the interval
+        A = numpy.zeros((num_stages, num_stages))
+        for i in range(num_stages):
+            qpts_i = qpts * c[i]
+            qwts_i = qwts * c[i]
+            Lvals_i = L.tabulate(0, qpts_i)[0, ]
+            A[i, :] = Lvals_i @ qwts_i
+
+        super(CollocationButcherTableau, self).__init__(A, b, c)
+
+
+class GaussLegendreButcherTableau(CollocationButcherTableau):
+    def __init__(self, num_stages):
+        assert num_stages > 0
+        U = FIAT.ufc_simplex(1)
+        L = FIAT.GaussLegendre(U, num_stages - 1)
+        super(GaussLegendreButcherTableau, self).__init__(L)
+
+
+class LobattoIIIAButcherTableau(CollocationButcherTableau):
+    def __init__(self, num_stages):
+        assert num_stages > 1
+        U = FIAT.ufc_simplex(1)
+        L = FIAT.GaussLobattoLegendre(U, num_stages - 1)
+        super(LobattoIIIAButcherTableau, self).__init__(L)
--- nonlinear.py
+++ nonlinear.py
@@ -1,15 +1,17 @@
-from firedrake import *
+from firedrake import *   # noqa F403
 
 import numpy
 from ufl import replace
+import butcher
+
 
 def getForm(F, butcher, t, dt, u0):
     # F: UFL
     # butcher: object with properties .A, .b, and .c
     # t and dt: Constant
     # u0: Function in the function space, current state.
-    """Given a variational form F(u; v) describing a nonlinear 
-    time-dependent problem (u_t, v, t) + F(u, t; v) and a 
+    """Given a variational form F(u; v) describing a nonlinear
+    time-dependent problem (u_t, v, t) + F(u, t; v) and a
     Butcher tableau, produce UFL for the s-stage RK method."""
 
     test, = F.arguments()
@@ -21,12 +23,12 @@ def getForm(F, butcher, t, dt, u0):
     c = butcher.c
 
     num_stages = A.shape[0]
-    
+
     # TODO: think about VectorFunctionSpace, which is more efficient
     # but breaks if V is itself a VectorFunctionSpace, I think.
     Vbig = numpy.prod([fs for i in range(num_stages)])
 
-    
+
     vnew = TestFunction(Vbig)
     k = Function(Vbig)
 
@@ -53,45 +55,23 @@ def getFormW(F, butcher, t, dt, u0):
     pass
 
 
-# TODO: Engineer better Butcher business, especially to get
-# general formula for families, or at least code some more up.
-
-class ButcherTableau(object):
-    def __init__(self):
-        self.A = []
-        self.b = []
-        self.c = []
-
-
-ButcherGL4 = ButcherTableau()
-ButcherGL4.A = numpy.array([[0.25, 0.25 - numpy.sqrt(3) / 6],
-                            [0.25+numpy.sqrt(3)/6, 0.25]])
-ButcherGL4.c = numpy.array([0.5-numpy.sqrt(3)/6, 0.5+numpy.sqrt(3)/6])
-ButcherGL4.b = numpy.array([0.5, 0.5])
-
-BE = ButcherTableau()
-BE.A = numpy.array([[1.0]])
-BE.b = numpy.array([1.0])
-BE.c = numpy.array([1.0])
-
 
 if __name__ == "__main__":
-    #  BT = ButcherGL4
-    BT = BE
+    BT = butcher.LobattoIIIAButcherTableau(2)
+
     ns = len(BT.b)
     N = 4
     msh = UnitSquareMesh(N, N)
-    #hierarchy = MeshHierarchy(msh, 1)
-    #msh = hierarchy[-1]
+
     V = FunctionSpace(msh, "CG", 1)
     x, y = SpatialCoordinate(msh)
 
-    # Stores initial condition!
-    u = interpolate(2.0 + sin(pi*x)*sin(pi*y), V)    
+    # Stores initial condition
+    u = interpolate(2.0 + sin(pi*x)*sin(pi*y), V)
     v = TestFunction(V)
 
     F = inner(grad(u), grad(v))*dx
-    
+
     dt = Constant(0.1)
     t = Constant(0.0)
 
@@ -104,15 +84,12 @@ if __name__ == "__main__":
     prob = NonlinearVariationalProblem(Fnew, k)
     solver = NonlinearVariationalSolver(prob, solver_parameters=params)
     print(assemble(u**2*dx))
-#    print(u.dat.data)
     solver.solve()
-    # This should look like the initial value but damped.  Something is wrong.
 
     unew = u.copy(deepcopy=True)
 
     for i in range(ns):
         unew.dat.data[:] += dt.values()[0] * BT.b[i] * k.dat[i].data
 
+    # And we've got damping!
     print(assemble(unew**2*dx))
-
-
