bd711bcfb370ac9869b35e4c6a4ba5d47b1fa6d8
Patrick Farrell <patrick@pefarrell.org>
2020-03-28 09:48:04 +0000

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+try:
+    import firedrake # noqa
+except ImportError:
+    raise Exception("Firedrake needs to be installed and activated. "
+                    "Please visit firedrakeproject.org")
+setup(
+    name='IRKsome',
+    version='0.0.1',
+    author='Rob Kirby, Jorge Marchena Menendez',
+    author_email='Robert_Kirby@baylor.edu',
+    description='A library for fully implicit Runge-Kutta methods in Firedrake',
+    long_description='',
+    packages=['IRKsome'],
+    zip_safe=False,
+)
