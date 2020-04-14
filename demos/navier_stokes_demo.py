f2963820e994cbac2371af5a0c7b6cfee7efe90f
Re-GeorX <jmarchenaster@gmail.com>
2020-04-14 17:09:47 -0500

    Navier-Stokes time dependent demo updated and working

--- demos/navier_stokes_demo.py
+++ demos/navier_stokes_demo.py
@@ -1,8 +1,8 @@
 from firedrake import *  # noqa: F403
-from IRKsome import GaussLegendre, getForm, Dt, TimeStepper
+from IRKsome import LobattoIIIC, getForm, Dt, TimeStepper
 import numpy
 
-ButcherTableau = GaussLegendre(1)
+ButcherTableau = LobattoIIIC(3)
 
 # Corners of the box.
 x0 = 0.0
@@ -12,19 +12,20 @@ y1 = 0.41
 
 #Mesh
 stepfile = "circle.step"
-h = 0.1 # Close to what Turek's benchmark has on coarsest mesh
+# Close to what Turek's time-dependent benchmark has on coarsest mesh
+h = 0.05 
 order = 2
-lvl=2
+lvl = 1
 mh = OpenCascadeMeshHierarchy(stepfile, element_size=h,
                               levels=lvl, order=order, cache=False,
                               verbose=True)
 
 #Space
-msh = mh[2]
+msh = mh[-1]
 
 
-V = VectorFunctionSpace(msh, "CG", 2)
-W = FunctionSpace(msh, "CG", 1)
+V = VectorFunctionSpace(msh, "CG", 4)
+W = FunctionSpace(msh, "DG", 3)
 Z = V * W
 
 v, w = TestFunctions(Z)
@@ -37,16 +38,19 @@ n= FacetNormal(msh)
 
 dt = Constant(1.0/1600)
 t = Constant(0.0)
-nu=0.001
+nu = Constant(0.001)
 rho=1.0
 r=0.05
 Umean=1.0
 L=0.1
 
 # define the variational form once outside the loop
+gamma = Constant(1.e2)
 F = (inner(Dt(u), v) * dx
-     + inner(dot(u, grad(u)), v) * dx
-     + nu*inner(grad(u), grad(v)) * dx - inner(p, div(v)) * dx
+     +inner(dot(grad(u), u), v) * dx
+     #+ gamma * inner(cell_avg(div(u)), cell_avg(div(v))) * dx
+     + nu * inner(grad(u), grad(v)) * dx
+     - inner(p, div(v)) * dx
      + inner(div(u), w) * dx)
 
 # boundary conditions are specified for each subspace
@@ -62,8 +66,10 @@ s_param={'ksp_type': 'preonly',
          'pc_type': 'lu',
          'pc_factor_shift_type': 'inblocks',
          'mat_type': 'aij',
-         'snes_max_it': 100,
-         'snes_monitor': None}
+         #'snes_monitor': None
+         }
+
+
 
 # the TimeStepper object builds the UFL for the multi-stage RK method
 # and sets up a solver.  It also provides a method for updating the solution
@@ -86,12 +92,13 @@ while (float(t) < 8):
     t.assign(float(t) + float(dt))
 
     # Numerical quantities
-    FD= assemble((nu*dot(grad(ut1), n)*n[1] - p*n[0])*ds(2) )
-    FL= assemble(-(nu*inner(grad(ut1), n)*n[0]-p*n[1])*ds(2) )
-    
-    CD+= [2 * FD / (Umean**2) / L] #Drag coefficient
-    CL+= [2 * FL / (Umean**2) / L] #Lift coefficient
+    FD= assemble((nu*dot(grad(ut1), n)*n[1] - p*n[0])*ds(5) )
+    FL= assemble(-(nu*inner(grad(ut1), n)*n[0]+p*n[1])*ds(5) )
+
+    CD+= [-2 * FD / (Umean**2) / L] #Drag coefficient
+    CL+= [-2 * FL / (Umean**2) / L] #Lift coefficient
     times.append(float(t))
+
 
 times = numpy.asarray(times)
 CD = numpy.asarray(CD)
@@ -117,3 +124,5 @@ print("t(CD_max)", tCDmax)
 print("CL_max", CLmax)
 print("t(CL_max)", tCLmax)
 
+#u, p = up.split()
+#print(p((0.15, .2)) - p((0.250, 0.2)))
