bbb0047b8a6466736d0489f8126579b89d7f2846
Scott MacLachlan <smaclachlan@mun.ca>
2021-05-16 19:52:10 -0230

    Adding test that should fail without fixes to interpolate/project logic

---
+++ tests/test_rtcf.py
@@ -0,0 +1,58 @@
+import pytest
+from firedrake import *
+from irksome import GaussLegendre, Dt, TimeStepper
+from ufl.algorithms.ad import expand_derivatives
+
+
+def curlcross(a, b):
+    curla = a[1].dx(0) - a[0].dx(1)
+    return as_vector([-curla*b[1], curla*b[0]])
+
+
+def RTCFtest(N, deg, butcher_tableau):
+
+    msh = UnitSquareMesh(N, N, quadrilateral=True)
+
+    Ve = FiniteElement("RTCF", msh.ufl_cell(), 2)
+    V = FunctionSpace(msh, Ve)
+
+    dt = Constant(0.1 / N)
+    t = Constant(0.0)
+
+    x, y = SpatialCoordinate(msh)
+
+    uexact = as_vector([t + 2*t*x + 4*t*y, 7*t + 5*t*x + 6*t*y])
+    rhs = expand_derivatives(diff(uexact, t)) + grad(div(uexact))
+
+    u = project(uexact, V)
+
+    v = TestFunction(V)
+    n = FacetNormal(msh)
+    F = inner(Dt(u), v)*dx + inner(div(u), div(v))*dx - inner(rhs, v)*dx
+    bc = DirichletBC(V, uexact, "on_boundary")
+
+    luparams = {"mat_type": "aij",
+                "ksp_type": "preonly",
+                "pc_type": "lu"}
+
+    stepper = TimeStepper(F, butcher_tableau, t, dt, u, bcs=bc,
+                          solver_parameters=luparams)
+
+    while (float(t) < 0.1):
+        if (float(t) + float(dt) > 0.1):
+            dt.assign(0.1 - float(t))
+        stepper.advance()
+        print(float(t))
+        t.assign(float(t) + float(dt))
+
+    return norm(u-uexact)
+
+
+@pytest.mark.parametrize(('deg', 'N', 'time_stages'),
+                         [(1, 2**j, i) for j in range(2, 4)
+                          for i in (1, 2)]
+                         + [(2, 2**j, i) for j in range(2, 4)
+                            for i in (2, 3)])
+def test_RTCF(deg, N, time_stages):
+    error = RTCFtest(N, deg, GaussLegendre(time_stages))
+    assert abs(error) < 1e-10
