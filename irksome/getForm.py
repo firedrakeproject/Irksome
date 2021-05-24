7748f30bb4e7f23dce1b25336b6cc8cb84ca466f
Rob Kirby <robert.c.kirby@gmail.com>
2021-05-24 16:39:18 -0500

    lint

--- irksome/getForm.py
+++ irksome/getForm.py
@@ -73,17 +73,18 @@ class DAEBCStageDataMixedSpace(BCStageData):
             gcur += Ainv[i, j] * replace(gorig, {t: t + c[j]*dt})
             try:
                 gdat = interpolate(gcur-u0_mult[i]*u0.sub(sub), V.sub(sub))
+
                 def gmethod(g, u):
                     # print("in gmethod")
                     # print(id(gdat))
                     # print(gdat.ufl_shape, g.ufl_shape, u.sub(sub).ufl_shape)
                     # print("end gmethod")
                     return gdat.interpolate(g-u0_mult[i]*u.sub(sub))
-            except:
+            except:  # noqa: E722
                 gdat = project(gcur-u0_mult[i]*u0.sub(sub), V.sub(sub))
                 gmethod = lambda g, u: gdat.project(g-u0_mult[i]*u.sub(sub))
         super().__init__(gdat, gcur, gmethod)
-    
+
 
 def getForm(F, butch, t, dt, u0, bcs=None, bc_type="DAE"):
     """Given a time-dependent variational form and a
--- tests/test_stokes.py
+++ tests/test_stokes.py
@@ -1,5 +1,4 @@
 import pytest
-import numpy as np
 from firedrake import *
 
 from irksome import Dt, TimeStepper, LobattoIIIC
@@ -10,7 +9,7 @@ from ufl.algorithms import expand_derivatives
 
 
 def StokesTest(N, butcher_tableau):
-    mesh = UnitSquareMesh(N,N)
+    mesh = UnitSquareMesh(N, N)
 
     Ve = VectorElement("CG", mesh.ufl_cell(), 2)
     Pe = FiniteElement("CG", mesh.ufl_cell(), 1)
@@ -19,9 +18,9 @@ def StokesTest(N, butcher_tableau):
 
     t = Constant(0.0)
     dt = Constant(1.0/N)
-    (x,y) = SpatialCoordinate(mesh)
+    (x, y) = SpatialCoordinate(mesh)
 
-    uexact = as_vector([x*t + y**2,-y*t+t*(x**2)])
+    uexact = as_vector([x*t + y**2, -y*t+t*(x**2)])
     pexact = Constant(0, domain=mesh)
 
     u_rhs = expand_derivatives(diff(uexact, t)) - div(grad(uexact)) + grad(pexact)
@@ -31,34 +30,29 @@ def StokesTest(N, butcher_tableau):
     test_z = TestFunction(Z)
     (u, p) = split(z)
     (v, q) = split(test_z)
-    F = (  inner(Dt(u),v)*dx
+    F = (inner(Dt(u), v)*dx
          + inner(grad(u), grad(v))*dx
          - inner(p, div(v))*dx
          - inner(q, div(u))*dx
          - inner(u_rhs, v)*dx
-         - inner(p_rhs, q)*dx
-        )
-    
-    bcs = [
-           DirichletBC(Z.sub(0), uexact, "on_boundary"),
-           DirichletBC(Z.sub(1), pexact, "on_boundary")
-          ]
+         - inner(p_rhs, q)*dx)
 
-    u,p = z.split()
+    bcs = [DirichletBC(Z.sub(0), uexact, "on_boundary"),
+           DirichletBC(Z.sub(1), pexact, "on_boundary")]
+
+    u, p = z.split()
     u.interpolate(uexact)
 
-    lu = {
-       "mat_type": "aij",
-       "snes_type": "newtonls",
-       "snes_linesearch_type": "l2",
-       "snes_linesearch_monitor": None,
-       "snes_monitor": None,
-       "snes_rtol": 1e-8,
-       "snes_atol": 1e-8,
-       "ksp_type": "preonly",
-       "pc_type": "lu",
-       "pc_factor_mat_solver_type": "mumps",
-         }
+    lu = {"mat_type": "aij",
+          "snes_type": "newtonls",
+          "snes_linesearch_type": "l2",
+          "snes_linesearch_monitor": None,
+          "snes_monitor": None,
+          "snes_rtol": 1e-8,
+          "snes_atol": 1e-8,
+          "ksp_type": "preonly",
+          "pc_type": "lu",
+          "pc_factor_mat_solver_type": "mumps"}
 
     stepper = TimeStepper(F, butcher_tableau, t, dt, z,
                           bcs=bcs, solver_parameters=lu)
@@ -69,8 +63,9 @@ def StokesTest(N, butcher_tableau):
         stepper.advance()
         t.assign(float(t) + float(dt))
 
-    (u,p) = z.split()
+    (u, p) = z.split()
     return errornorm(uexact, u)
+
 
 @pytest.mark.parametrize(('N', 'time_stages'),
                          [(2**j, i) for j in range(2, 4)
