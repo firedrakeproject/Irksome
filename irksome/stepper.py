763f18d0cfcc653181ac6211b2114b3bb6487434
Rob Kirby <robert.c.kirby@gmail.com>
2021-07-19 09:55:16 -0500

    Don't need persistent handle on splitting function

--- irksome/stepper.py
+++ irksome/stepper.py
@@ -40,7 +40,6 @@ class TimeStepper:
     """
     def __init__(self, F, butcher_tableau, t, dt, u0, bcs=None,
                  solver_parameters=None, bc_type="DAE", splitting=AI):
-        self.splitting = splitting
         self.u0 = u0
         self.t = t
         self.dt = dt
