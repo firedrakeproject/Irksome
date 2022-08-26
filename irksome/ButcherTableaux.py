b3a922e840c984d3b42033646bb0a31ceb59cf7c
Rob Kirby <robert.c.kirby@gmail.com>
2022-08-26 14:03:42 -0500

    add queries for implicit/explicitness

--- irksome/ButcherTableaux.py
+++ irksome/ButcherTableaux.py
@@ -41,6 +41,34 @@ class ButcherTableau(object):
         except numpy.linalg.LinAlgError:
             return False
 
+    @property
+    def is_explicit(self):
+        A = self.A
+        ns = self.num_stages
+        for i in range(ns):
+            for j in range(i, ns):
+                if abs(A[i, j]) > 1.e-15:
+                    return False
+        return True
+
+    @property
+    def is_diagonally_implicit(self):
+        A = self.A
+        ns = self.num_stages
+        for i in range(ns):
+            for j in range(i+1, ns):
+                if abs(A[i, j]) > 1.e-15:
+                    return False
+        return True
+
+    @property
+    def is_implicit(self):
+        return not self.is_explicit
+
+    @property
+    def is_fully_implicit(self):
+        return self.is_implicit and not self.is_diagonally_implicit
+        
     def __str__(self):
         return str(self.__class__).split(".")[-1][:-2]+"()"
 
