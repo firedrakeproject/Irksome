d5db40a5b721d671421593983d7896d479e71b4f
Scott MacLachlan <smaclachlan@mun.ca>
2022-09-06 16:29:56 -0230

    Adding Neumann BC test

--- tests/test_dirk.py
+++ tests/test_dirk.py
@@ -62,5 +62,44 @@ def test_1d_heat_dirichletbc(butcher_tableau):
         # assert isclose(u.at(x0), u_0)
         # assert isclose(u.at(x1), u_1)
 
+@pytest.mark.parametrize("butcher_tableau", [Alexander()])
+def test_1d_heat_neumannbc(butcher_tableau):
+
+    N = 10
+    msh = IntervalMesh(N, x1)
+    V = FunctionSpace(msh, "CG", 1)
+    dt = Constant(1.0 / N)
+    t = Constant(0.0)
+    (x,) = SpatialCoordinate(msh)
+
+    uexact = cos(pi*x)*exp(-(pi**2)*t)
+    rhs = expand_derivatives(diff(uexact, t)) - div(grad(uexact))
+    u = interpolate(uexact, V)
+    v = TestFunction(V)
+    F = (
+        inner(Dt(u), v) * dx
+        + inner(grad(u), grad(v)) * dx
+        - inner(rhs, v) * dx
+    )
+    bc = [
+    ]
+
+    luparams = {"mat_type": "aij", "ksp_type": "preonly", "pc_type": "lu"}
+
+    stepper = DIRKTimeStepper(
+        F, butcher_tableau, t, dt, u, bcs=bc, solver_parameters=luparams
+    )
+
+    t_end = 2.0
+    while float(t) < t_end:
+        if float(t) + float(dt) > t_end:
+            dt.assign(t_end - float(t))
+        stepper.advance()
+        t.assign(float(t) + float(dt))
+        # Check solution and boundary values
+        print(errornorm(uexact, u) / norm(uexact))
+        # assert norm(u - uexact) / norm(uexact) < 10.0 ** -5
+
 
 test_1d_heat_dirichletbc(Alexander())
+test_1d_heat_neumannbc(Alexander())
