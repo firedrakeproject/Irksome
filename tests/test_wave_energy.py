58c40c7f1a9fd2e010440fe55355d1f6547ba10f
Rob Kirby <robert.c.kirby@gmail.com>
2024-07-26 22:01:16 -0500

    Add a family of PEP RK methods from the Ketcheson paper

--- tests/test_wave_energy.py
+++ tests/test_wave_energy.py
@@ -11,7 +11,7 @@ from irksome.tools import AI, IA
 # various time steppers.
 
 
-def wave(n, deg, butcher_tableau, splitting=AI):
+def wave(n, deg, butcher_tableau, **kwargs):
     N = 2**n
     msh = UnitIntervalMesh(N)
 
@@ -63,5 +63,18 @@ def wave(n, deg, butcher_tableau, splitting=AI):
                          + [(2, i) for i in (2, 3)])
 def test_wave_eq(deg, N, time_stages, splitting):
     energy = wave(N, deg, GaussLegendre(time_stages), splitting)
+    print(energy)
+    assert np.allclose(energy[1:], energy[:-1])
+
+
+@pytest.mark.parametrize('N', [2**j for j in range(2, 4)])
+@pytest.mark.parametrize('deg', (1, 2))
+@pytest.mark.parameterize('pep', ((4, 2, 5),
+                                  (5, 2, 6),
+                                  (6, 3, 6),
+                                  (7, 4, 6),
+                                  (7, 5, 6)))
+def test_pep_wave_eq(deg, N, time_stages, splitting):
+    energy = wave(N, deg, PEPRK(time_stages), splitting)
     print(energy)
     assert np.allclose(energy[1:], energy[:-1])
