f79e114642fb8c4edda6502c8106b52d55fde697
Robert Kirby <robert.c.kirby@gmail.com>
2024-12-10 16:58:42 -0600

    are we ready for the moment of truth?  Probably not

--- irksome/galerkin_stepper.py
+++ irksome/galerkin_stepper.py
@@ -6,13 +6,13 @@ from ufl.constantvalue import as_ufl
 from .bcs import bc2space, stage2spaces4bc
 from .deriv import TimeDerivative
 from .stage import getBits
-from .tools import MeshConstant, getNullSpace, replace
+from .tools import MeshConstant, getNullspace, replace
 import np
 from firedrake import TestFunction, Function, NonlinearVariationalProblem as NLVP, NonlinearVariationalSolver as NLVS
+from firedrake.dmhooks import pop_parent, push_parent
 
 
-def getFormGalerkin(F, L_trial, L_test, Q, t, dt, u0,
-                    bcs=None, nullspace=None):
+def getFormGalerkin(F, L_trial, L_test, Q, t, dt, u0, bcs=None, nullspace=None):
 
     """Given a time-dependent variational form, trial and test spaces, and
     a quadrature rule, produce UFL for the Galerkin-in-Time method.
@@ -40,7 +40,7 @@ def getFormGalerkin(F, L_trial, L_test, Q, t, dt, u0,
     On output, we return a tuple consisting of four parts:
 
        - Fnew, the :class:`Form`
-       - w
+       - UU, the :class:`Function` representing the stages to be solved for
        - `bcnew`, a list of :class:`firedrake.DirichletBC` objects to be posed
          on the Galerkin-in-time solution,
        - 'nspnew', the :class:`firedrake.MixedVectorSpaceBasis` object
@@ -166,103 +166,127 @@ def getFormGalerkin(F, L_trial, L_test, Q, t, dt, u0,
             Vbigi = stage2spaces4bc(bc, V, Vbig, i)
             bcsnew.append(bc.reconstruct(V=Vbigi, g=bc_func_for_stages[i]))
 
+    return Fnew, UU, bcsnew, getNullspace(V, Vbig, num_stages, nullspace)
 
-# class GalkerinTimeStepper:
-#     """Front-end class for advancing a time-dependent PDE via a Galerkin
-#     in time method
 
-#     :arg F: A :class:`ufl.Form` instance describing the semi-discrete problem
-#             F(t, u; v) == 0, where `u` is the unknown
-#             :class:`firedrake.Function and `v` is the
-#             :class:firedrake.TestFunction`.
-#     :arg V_trial: A :class:`firedrake.FunctionSpace` for the trial functions in time
-#     :arg V_test: A :class:`firedrake.FunctionSpace` for the test functions in time
-#     :arg quad: A :class:`fiat.QuadratureRule` for the time integration
-#     :arg t: a :class:`Function` on the Real space over the same mesh as
-#          `u0`.  This serves as a variable referring to the current time.
-#     :arg dt: a :class:`Function` on the Real space over the same mesh as
-#          `u0`.  This serves as a variable referring to the current time step.
-#          The user may adjust this value between time steps.
-#     :arg u0: A :class:`firedrake.Function` containing the current
-#             state of the problem to be solved.
-#     :arg bcs: An iterable of :class:`firedrake.DirichletBC` containing
-#             the strongly-enforced boundary conditions.  Irksome will
-#             manipulate these to obtain boundary conditions for each
-#             stage of the method.
-#     :arg solver_parameters: A :class:`dict` of solver parameters that
-#             will be used in solving the algebraic problem associated
-#             with each time step.
-#     :arg appctx: An optional :class:`dict` containing application context.
-#             This gets included with particular things that Irksome will
-#             pass into the nonlinear solver so that, say, user-defined preconditioners
-#             have access to it.
-#     :arg nullspace: A list of tuples of the form (index, VSB) where
-#             index is an index into the function space associated with
-#             `u` and VSB is a :class: `firedrake.VectorSpaceBasis`
-#             instance to be passed to a
-#             `firedrake.MixedVectorSpaceBasis` over the larger space
-#             associated with the Runge-Kutta method
-#     """
-#     def __init__(self, F, V_trial, V_test, quad, t, dt, u0, bcs=None,
-#                  solver_parameters=None, appctx=None, nullspace=None):
-#         self.u0 = u0
-#         self.F = F
-#         self.orig_bcs = bcs
-#         self.t = t
-#         self.dt = dt
-#         self.V_trial = V_trial
-#         self.V_test = V_test
-#         self.quad = quad
-#         self.num_steps = 0
-#         self.num_nonlinear_iterations = 0
-#         self.num_linear_iterations = 0
+class GalkerinTimeStepper:
+    """Front-end class for advancing a time-dependent PDE via a Galerkin
+    in time method
 
-#         bigF, bigBasis, bigBCs, bigNSP = \
-#             getFormGalerkin(F, V_trial, V_test, quad, t, dt, u0, bcs, nullspace)
+    :arg F: A :class:`ufl.Form` instance describing the semi-discrete problem
+            F(t, u; v) == 0, where `u` is the unknown
+            :class:`firedrake.Function and `v` is the
+            :class:firedrake.TestFunction`.
+    :arg t: a :class:`Function` on the Real space over the same mesh as
+         `u0`.  This serves as a variable referring to the current time.
+    :arg dt: a :class:`Function` on the Real space over the same mesh as
+         `u0`.  This serves as a variable referring to the current time step.
+         The user may adjust this value between time steps.
+    :arg u0: A :class:`firedrake.Function` containing the current
+            state of the problem to be solved.
+    :arg bcs: An iterable of :class:`firedrake.DirichletBC` containing
+            the strongly-enforced boundary conditions.  Irksome will
+            manipulate these to obtain boundary conditions for each
+            stage of the method.
+    :arg solver_parameters: A :class:`dict` of solver parameters that
+            will be used in solving the algebraic problem associated
+            with each time step.
+    :arg appctx: An optional :class:`dict` containing application context.
+            This gets included with particular things that Irksome will
+            pass into the nonlinear solver so that, say, user-defined preconditioners
+            have access to it.
+    :arg nullspace: A list of tuples of the form (index, VSB) where
+            index is an index into the function space associated with
+            `u` and VSB is a :class: `firedrake.VectorSpaceBasis`
+            instance to be passed to a
+            `firedrake.MixedVectorSpaceBasis` over the larger space
+            associated with the Runge-Kutta method
+    """
+    def __init__(self, F, order, t, dt, u0, bcs=None, basis_type=None,
+                 quadrature=None,
+                 solver_parameters=None, appctx=None, nullspace=None):
+        assert order >= 0
+        self.u0 = u0
+        self.orig_bcs = bcs
+        self.t = t
+        self.dt = dt
+        self.order = order
+        if basis_type is None:
+            basis_type = "Lagrange"
+        self.basis_type = basis_type
 
-#         self.bigBasis = bigBasis
-#         self.bigBCs = bigBCs
-#         problem = NLVP(bigF, bigBasis, bigBCs)
-#         appctx_irksome = {"F": F,
-#                           "t": t,
-#                           "dt": dt,
-#                           "u0": u0,
-#                           "bcs": bcs,
-#                           "nullspace": nullspace}
-#         if appctx is None:
-#             appctx = appctx_irksome
-#         else:
-#             appctx = {**appctx, **appctx_irksome}
+        V = u0.function_space()
+        self.num_fields = len(V)
 
-#         push_parent(u0.function_space().dm, stages.function_space().dm)
-#         self.solver = NLVS(problem,
-#                            appctx=appctx,
-#                            solver_parameters=solver_parameters,
-#                            nullspace=bigNSP)
-#         pop_parent(u0.function_space().dm, stages.function_space().dm)
+        ufc_line = FIAT.reference_element.ufc_simplex(1)
 
-#     def _update(self):
-#         """Assuming the algebraic problem for the Galerkin problems has been
-#         solved, updates the solution.  This will not typically be
-#         called by an end user."""
+        if basis_type == "Lagrange":
+            elgetter = FIAT.Lagrange
+        elif basis_type == "Bernstein":
+            assert order >= 1
+            elgetter = FIAT.Bernstein
+        else:
+            raise NotImplementedError("Not implemented basis type")
 
-#         # ws = self.bigBasis
-#         # u0bits = u0.subfunctions
-#         # for s in range(ns):
-#         #     for i, u0bit in enumerate(u0bits):
-#         #         u0bit =
+        self.trial_el = elgetter(ufc_line, order)
+        self.test_el = elgetter(ufc_line, order-1)
 
-#     def advance(self):
-#         """Advances the system from time `t` to time `t + dt`.
-#         Note: overwrites the value `u0`."""
-#         push_parent(self.u0.function_space().dm, self.stages.function_space().dm)
-#         self.solver.solve()
-#         pop_parent(self.u0.function_space().dm, self.stages.function_space().dm)
+        if quadrature is None:
+            quadrature = FIAT.quadrature.make_quadrature(ufc_line, order)
+        assert isinstance(quadrature, FIAT.quadrature.QuadratureRule)
+        self.quadrature = quadrature
 
-#         self.num_steps += 1
-#         self.num_nonlinear_iterations += self.solver.snes.getIterationNumber()
-#         self.num_linear_iterations += self.solver.snes.getLinearSolveIterations()
-#         self._update()
+        self.num_steps = 0
+        self.num_nonlinear_iterations = 0
+        self.num_linear_iterations = 0
 
-#     def solver_stats(self):
-#         return (self.num_steps, self.num_nonlinear_iterations, self.num_linear_iterations)
+        bigF, UU, bigBCs, bigNSP = \
+            getFormGalerkin(F, self.trial_el, self.test_el,
+                            quadrature, t, dt, u0, bcs, nullspace)
+
+        self.UU = UU
+        self.bigBCs = bigBCs
+        problem = NLVP(bigF, UU, bigBCs)
+        appctx_irksome = {"F": F,
+                          "t": t,
+                          "dt": dt,
+                          "u0": u0,
+                          "bcs": bcs,
+                          "nullspace": nullspace}
+        if appctx is None:
+            appctx = appctx_irksome
+        else:
+            appctx = {**appctx, **appctx_irksome}
+
+        push_parent(u0.function_space().dm, UU.function_space().dm)
+        self.solver = NLVS(problem,
+                           appctx=appctx,
+                           solver_parameters=solver_parameters,
+                           nullspace=bigNSP)
+        pop_parent(u0.function_space().dm, UU.function_space().dm)
+
+    def _update(self):
+        """Assuming the algebraic problem for the Galerkin problems has been
+        solved, updates the solution.  This will not typically be
+        called by an end user."""
+        u0 = self.u0
+        u0bits = u0.subfunctions
+        UUs = self.UU.subfunctions
+
+        for i, u0bit in enumerate(u0bits):
+            u0bit.assign(UUs[self.num_fields*(self.order-1)+i])
+
+    def advance(self):
+        """Advances the system from time `t` to time `t + dt`.
+        Note: overwrites the value `u0`."""
+        push_parent(self.u0.function_space().dm, self.stages.function_space().dm)
+        self.solver.solve()
+        pop_parent(self.u0.function_space().dm, self.stages.function_space().dm)
+
+        self.num_steps += 1
+        self.num_nonlinear_iterations += self.solver.snes.getIterationNumber()
+        self.num_linear_iterations += self.solver.snes.getLinearSolveIterations()
+        self._update()
+
+    def solver_stats(self):
+        return (self.num_steps, self.num_nonlinear_iterations, self.num_linear_iterations)
