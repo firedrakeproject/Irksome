79818a283bfff49a7a3272bac9c1e9e30165ba63
Robert C. Kirby <robert.kirby@gmail.com>
2025-02-27 17:18:05 -0600

    put CG time stepper into hierarchy

--- irksome/galerkin_stepper.py
+++ irksome/galerkin_stepper.py
@@ -1,19 +1,17 @@
-from functools import reduce
 from FIAT import (Bernstein, DiscontinuousElement, DiscontinuousLagrange,
                   IntegratedLegendre, Lagrange, Legendre,
                   make_quadrature, ufc_simplex)
-from operator import mul
 from ufl import zero
 from ufl.constantvalue import as_ufl
+from .base_time_stepper import StageCoupledTimeStepper
 from .bcs import bc2space, stage2spaces4bc
 from .deriv import TimeDerivative
-from .tools import getNullspace, ConstantOrZero, component_replace, replace
+from .tools import ConstantOrZero, component_replace, replace
 import numpy as np
-from firedrake import as_vector, dot, Constant, TestFunction, Function, NonlinearVariationalProblem as NLVP, NonlinearVariationalSolver as NLVS
-from firedrake.dmhooks import pop_parent, push_parent
+from firedrake import as_vector, dot, Constant, TestFunction
 
 
-def getFormGalerkin(F, L_trial, L_test, Q, t, dt, u0, bcs=None):
+def getFormGalerkin(F, L_trial, L_test, Q, t, dt, u0, stages, bcs=None):
 
     """Given a time-dependent variational form, trial and test spaces, and
     a quadrature rule, produce UFL for the Galerkin-in-Time method.
@@ -53,9 +51,9 @@ def getFormGalerkin(F, L_trial, L_test, Q, t, dt, u0, bcs=None):
 
     num_stages = L_test.space_dimension()
 
-    Vbig = reduce(mul, (V for _ in range(num_stages)))
+    Vbig = stages.function_space()
     VV = TestFunction(Vbig)
-    UU = Function(Vbig)  # u0 + this are coefficients of the Galerkin polynomial
+    UU = stages
 
     u_np = np.reshape(UU, (num_stages, *u0.ufl_shape))
     v_np = np.reshape(VV, (num_stages, *u0.ufl_shape))
@@ -123,10 +121,10 @@ def getFormGalerkin(F, L_trial, L_test, Q, t, dt, u0, bcs=None):
             Vbigi = stage2spaces4bc(bc, V, Vbig, i)
             bcsnew.append(bc.reconstruct(V=Vbigi, g=bc_func_for_stages[i]))
 
-    return Fnew, UU, bcsnew
+    return Fnew, bcsnew
 
 
-class GalerkinTimeStepper:
+class GalerkinTimeStepper(StageCoupledTimeStepper):
     """Front-end class for advancing a time-dependent PDE via a Galerkin
     in time method
 
@@ -202,55 +200,14 @@ class GalerkinTimeStepper:
         self.quadrature = quadrature
         assert np.size(quadrature.get_points()) >= order
 
-        self.num_steps = 0
-        self.num_nonlinear_iterations = 0
-        self.num_linear_iterations = 0
+        super().__init__(F, t, dt, u0, order, bcs=bcs,
+                         solver_parameters=solver_parameters,
+                         appctx=appctx, nullspace=nullspace)
 
-        bigF, UU, bigBCs = \
-            getFormGalerkin(F, self.trial_el, self.test_el,
-                            quadrature, t, dt, u0, bcs)
+    def get_form_and_bcs(self, stages):
+        return getFormGalerkin(self.F, self.trial_el, self.test_el,
+                               self.quadrature, self.t, self.dt, self.u0, stages, self.orig_bcs)
 
-        bigNSP = getNullspace(V, UU.function_space(),
-                              order, nullspace)
-
-        self.UU = UU
-        self.bigBCs = bigBCs
-        problem = NLVP(bigF, UU, bigBCs)
-        appctx_irksome = {"F": F,
-                          "t": t,
-                          "dt": dt,
-                          "u0": u0,
-                          "bcs": bcs,
-                          "nullspace": nullspace}
-        if appctx is None:
-            appctx = appctx_irksome
-        else:
-            appctx = {**appctx, **appctx_irksome}
-
-        push_parent(u0.function_space().dm, UU.function_space().dm)
-        self.solver = NLVS(problem,
-                           appctx=appctx,
-                           solver_parameters=solver_parameters,
-                           nullspace=bigNSP)
-        pop_parent(u0.function_space().dm, UU.function_space().dm)
-
-    def advance(self):
-        """Advances the system from time `t` to time `t + dt`.
-        Note: overwrites the value `u0`."""
-        push_parent(self.u0.function_space().dm, self.UU.function_space().dm)
-        self.solver.solve()
-        pop_parent(self.u0.function_space().dm, self.UU.function_space().dm)
-
-        self.num_steps += 1
-        self.num_nonlinear_iterations += self.solver.snes.getIterationNumber()
-        self.num_linear_iterations += self.solver.snes.getLinearSolveIterations()
-
-        u0 = self.u0
-        u0bits = u0.subfunctions
-        UUs = self.UU.subfunctions
-
-        for i, u0bit in enumerate(u0bits):
-            u0bit.assign(UUs[self.num_fields*(self.order-1)+i])
-
-    def solver_stats(self):
-        return (self.num_steps, self.num_nonlinear_iterations, self.num_linear_iterations)
+    def _update(self):
+        for i, u0bit in enumerate(self.u0.subfunctions):
+            u0bit.assign(self.stages.subfunctions[self.num_fields*(self.order-1)+i])
