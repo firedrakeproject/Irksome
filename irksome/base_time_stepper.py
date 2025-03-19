4c5337228de0065f44ac054d53c6a59d95727289
Josh Hope-Collins <jhc.jss@gmail.com>
2025-03-19 12:24:57 +0000

    Update base_time_stepper.py

--- irksome/base_time_stepper.py
+++ irksome/base_time_stepper.py
@@ -137,7 +137,7 @@ class StageCoupledTimeStepper(BaseTimeStepper):
         Note: overwrites the value `u0`."""
 
         push_parent(self.u0.function_space().dm, self.stages.function_space().dm)
-        if bounds is None:
+        if self.stage_bounds is None:
             self.solver.solve()
         else:
             self.solver.solve(bounds=self.stage_bounds)
