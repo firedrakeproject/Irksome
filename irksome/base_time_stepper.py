9f7869a1a457e9f190de0cc1075de2e7bcbd056d
John Stephens <john_stephens2@baylor.edu>
2025-04-09 15:52:52 -0500

    more whitespace

--- irksome/base_time_stepper.py
+++ irksome/base_time_stepper.py
@@ -86,10 +86,10 @@ class StageCoupledTimeStepper(BaseTimeStepper):
             the Runge-Kutta method to be used for time marching.
     :arg bounds: An optional kwarg used in certain bounds-constrained methods.
     :arg use_collocation_update: An optional kwarg indicating whether to use
-        the terminal value of the collocation polynomial as the solution 
-        update. This is needed to bypass the mass matrix inversion when 
-        enforcing bounds constraints with an RK method that is not stiffly 
-        accurate. Currenty, only constant-in-time boundary conditions are 
+        the terminal value of the collocation polynomial as the solution
+        update. This is needed to bypass the mass matrix inversion when
+        enforcing bounds constraints with an RK method that is not stiffly
+        accurate. Currenty, only constant-in-time boundary conditions are
         supported.
     """
     def __init__(self, F, t, dt, u0, num_stages,
