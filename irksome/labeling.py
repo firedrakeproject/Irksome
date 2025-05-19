3138c49f2c59540afc5d366b4a5bb9487abafe1d
Robert Kirby <robert.c.kirby@gmail.com>
2025-05-19 10:54:23 +0100

    Make labels work with IMEX (#149)
    * Make labels work with IMEX
    
    ---------
    
    Co-authored-by: Pablo Brubeck <brubeck@protonmail.com>

---
+++ irksome/labeling.py
@@ -0,0 +1,17 @@
+from firedrake.fml import Label, keep, drop, LabelledForm
+
+
+explicit = Label("explicit")
+
+
+def split_explicit(F):
+    if not isinstance(F, LabelledForm):
+        return (F, None)
+    exp_part = F.label_map(lambda t: t.has_label(explicit),
+                           map_if_true=keep,
+                           map_if_false=drop)
+
+    imp_part = F.label_map(lambda t: t.labels == {},
+                           map_if_true=keep, map_if_false=drop)
+
+    return imp_part.form, exp_part.form
