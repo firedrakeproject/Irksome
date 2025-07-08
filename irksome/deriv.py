d615eb010675dece8ce49d8f2791d774dd2b445c
Pablo Brubeck <brubeck@protonmail.com>
2025-07-08 17:54:06 +0100

    Backwards compatibility with MultiFunction

--- irksome/deriv.py
+++ irksome/deriv.py
@@ -2,8 +2,20 @@ from functools import singledispatchmethod
 
 from ufl.constantvalue import as_ufl
 from ufl.core.ufl_type import ufl_type
-from ufl.corealg.dag_traverser import DAGTraverser
-from ufl.algorithms.map_integrands import map_integrands
+
+# NOTE we support the old MultiFunction and the new DAGTraverser APIs
+# for differentiation. The full switch to DAGTraverser will occur
+# after the new API gets merged into the UFL release branch.
+try:
+    from ufl.corealg.dag_traverser import DAGTraverser
+    from ufl.algorithms.map_integrands import map_integrands
+    apply_rule = lambda rule, expr: rule(expr)
+except ImportError:
+    from ufl.corealg.multifunction import MultiFunction as DAGTraverser
+    from ufl.algorithms.map_integrands import map_integrand_dags as map_integrands
+    from ufl.algorithms.map_integrands import map_expr_dag as apply_rule
+    DAGTraverser.postorder = lambda x: x
+
 from ufl.algorithms.apply_derivatives import GenericDerivativeRuleset
 from ufl.algorithms.apply_algebra_lowering import apply_algebra_lowering
 from ufl.form import BaseForm
@@ -55,7 +67,7 @@ class TimeDerivativeRuleset(GenericDerivativeRuleset):
 
     @process.register(ConstantValue)
     @process.register(Variable)
-    def constant(self, o):
+    def _terminal(self, o):
         if self.t is not None and o is self.t:
             return self._Id
         else:
@@ -80,7 +92,7 @@ class TimeDerivativeRuleset(GenericDerivativeRuleset):
         if isinstance(f, TimeDerivative):
             return TimeDerivative(f)
         else:
-            return self(f)
+            return apply_rule(self, f)
 
     @process.register(Conj)
     @process.register(Curl)
@@ -91,16 +103,24 @@ class TimeDerivativeRuleset(GenericDerivativeRuleset):
     @process.register(ReferenceGrad)
     @process.register(ReferenceValue)
     @DAGTraverser.postorder
-    def linear_op(self, o, *operands):
+    def _linear_op(self, o, *operands):
         return o._ufl_expr_reconstruct_(*operands)
+
+    # TODO MultiFunction backwards compatibility
+    constant_value = _terminal
+    variable = _terminal
+    indexed = _linear_op
+    derivative = _linear_op
+    grad = _linear_op
+    curl = _linear_op
+    div = _linear_op
 
 
 # mapping rules to splat out time derivatives so that replacement should
 # work on more complex problems.
 class TimeDerivativeRuleDispatcher(DAGTraverser):
-    def __init__(self, t=None, timedep_coeffs=None,
-                 compress=True, visited_cache=None, result_cache=None):
-        super().__init__(compress=compress, visited_cache=visited_cache, result_cache=result_cache)
+    def __init__(self, t=None, timedep_coeffs=None, **kwargs):
+        super().__init__(**kwargs)
         self.rules = TimeDerivativeRuleset(t=t, timedep_coeffs=timedep_coeffs)
 
     # Work around singledispatchmethod inheritance issue;
@@ -112,12 +132,15 @@ class TimeDerivativeRuleDispatcher(DAGTraverser):
     @process.register(TimeDerivative)
     def time_derivative(self, o):
         f, = o.ufl_operands
-        return self.rules(f)
+        return apply_rule(self.rules, f)
 
     @process.register(Expr)
     @process.register(BaseForm)
-    def ufl_type(self, o):
+    def _generic(self, o):
         return self.reuse_if_untouched(o)
+
+    # TODO MultiFunction backwards compatibility
+    ufl_type = DAGTraverser.reuse_if_untouched
 
 
 def apply_time_derivatives(expression, t=None, timedep_coeffs=None):
