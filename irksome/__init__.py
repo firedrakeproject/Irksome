262e6d0a187dfed67f3a6fb2af01f562f69b181a
436ahsan <436ahsan@gmail.com>
2025-09-10 15:37:27 -0500

    update init to import KronPC subclasses

--- irksome/__init__.py
+++ irksome/__init__.py
@@ -31,5 +31,5 @@ from .tools import MeshConstant             # noqa: F401
 from .wso_dirk_tableaux import WSODIRK      # noqa: F401
 from .galerkin_stepper import GalerkinTimeStepper  # noqa: F401
 from .discontinuous_galerkin_stepper import DiscontinuousGalerkinTimeStepper  # noqa: F401
-from .KronPC import KronPC, MassKronPC, StiffnessKronPC # noqa: F401
+from .KronPC import KronPC, MassKronPC, StiffnessKronPC, SIPGStiffnessKronPC # noqa: F401
 
