c1da2c85debedd3f156344235dda6625db6d3165
John Stephens <john_stephens2@baylor.edu>
2025-11-12 17:30:51 -0600

    initial multistep functionality and tests

---
+++ irksome/multistep.py
@@ -0,0 +1,191 @@
+from .tools import replace, vecconst, getNullspace
+from .manipulation import extract_terms, strip_dt_form
+from .deriv import expand_time_derivatives
+from .stepper import TimeStepper, valid_base_kwargs
+from .base_time_stepper import BaseTimeStepper
+from ufl.constantvalue import as_ufl
+from ufl import zero
+from firedrake import NonlinearVariationalProblem, NonlinearVariationalSolver
+
+
+class MultistepStepper(BaseTimeStepper):
+    
+    """front-end class for advancing time-dependent PDE via the BDF2 Method
+    
+    :arg F: A :class:`ufl.Form` instance describing the semi-discrete problem
+        F(t, u; v) == 0, where `u` is the unknown
+        :class:`firedrake.Function and `v` is the
+        :class:firedrake.TestFunction`.
+    :arg t: a :class:`Function` on the Real space over the same mesh as
+         `u0`.  This serves as a variable referring to the current time.
+    :arg dt: a :class:`Function` on the Real space over the same mesh as
+         `u0`.  This serves as a variable referring to the current time step.
+         The user may adjust this value between time steps.
+    :arg a: An array of length s + 1 containing the left-hand coefficients of the method (usually, a[-1] = 1).
+    :arg b: An array of length s + 1 containing the right-hand coefficients of the method    
+    :arg u: A :class:`firedrake.Function` containing the current
+            state of the problem to be solved.
+    :arg bcs: An iterable of :class:`firedrake.DirichletBC` containing
+            the strongly-enforced boundary conditions.
+    :arg solver_parameters: A :class:`dict` of solver parameters that
+            will be used in solving the algebraic problem associated
+            with each time step.
+    :arg appctx: An optional :class:`dict` containing application context.
+            This gets included with particular things that Irksome will
+            pass into the nonlinear solver so that, say, user-defined preconditioners
+            have access to it.
+    :arg startup_tableau: An optional :class:`ButcherTableau` representing a single-step method to be used 
+            to find the required starting values.
+    :arg startup_dt_div: An optional integer for the startup method. Startup uses dt = dt / startup_dt_div.
+    :arg startup_solver_parameters: An optional :class:`dict` of solver parameters to be passed to the startup method.
+    """
+
+    def __init__(self, F, t, dt, u0, a, b, bcs=None, solver_parameters=None, bounds=None, appctx=None, nullspace=None,
+                 transpose_nullspace=None, near_nullspace=None, startup_params=None, **kwargs):
+
+        super().__init__(F, t, dt, u0,
+                         bcs=bcs, appctx=appctx, nullspace=nullspace)
+        
+        self.s = len(b) - 1
+        self.a = vecconst(a)
+        self.b = vecconst(b)
+        self.us = [u0.copy(deepcopy=True) for const in self.a[0:-1] if const != zero()]
+        self.us.append(u0)
+        Fnew, bcsnew = self.get_form_and_bcs(F, t, dt, u0, self.a, self.b, bcs=bcs)
+
+        self.problem = NonlinearVariationalProblem(Fnew, self.us[-1], bcs=bcsnew, form_compiler_parameters=kwargs.pop("form_compiler_parameters", None),
+            is_linear=kwargs.pop("is_linear", False),
+            restrict=kwargs.pop("restrict", False))
+
+        self.solver = NonlinearVariationalSolver(
+            self.problem, appctx=self.appctx,
+            nullspace=nullspace,
+            transpose_nullspace=transpose_nullspace,
+            near_nullspace=near_nullspace,
+            solver_parameters=solver_parameters,
+            **kwargs
+            )
+
+        self.num_steps = 0
+        self.num_nonlinear_iterations = 0
+        self.num_linear_iterations = 0
+
+        if bool(startup_params): 
+            print(startup_params)
+            self.mechanized_startup(F, t, dt, u0, startup_params, bcs=bcs)
+
+        self.bounds = bounds
+
+
+    def get_form_and_bcs(self, F, t, dt, u0, a, b, bcs=None):
+
+        F = expand_time_derivatives(F, t=t, timedep_coeffs=(u0, ))
+        v, = F.arguments()
+        V = v.function_space()
+
+        assert V == u0.function_space()
+
+        #################################################
+        ## Is this the proper generalization?
+        split_form = extract_terms(F)
+        F_dtless = strip_dt_form(split_form.time) ## do I need to strip_dt_form() this? if so, what do I replace?
+        F_remainder = split_form.remainder
+
+        # replace the time derivative with a linear combination of the previous steps
+        temp_form = zero()
+        step_number = 0
+        for coeff in a:
+            if coeff is zero():
+                pass
+            else:
+               temp_form += coeff * self.us[step_number]
+               step_number += 1
+
+        Fnew = replace(F_dtless, {u0: temp_form})
+        #################################################
+
+        # form the right hand side
+        for (i, coeff) in enumerate(b):
+            if coeff is zero():
+                pass
+            else:
+                Fnew += dt * coeff * replace(F_remainder, {t: t + (i - (self.s - 1)) * dt})
+
+        if bcs is None:
+            bcs = []
+        bcsnew = []
+
+        # grab boundary conditions at t + dt
+        for bc in bcs:
+            g0 = as_ufl(bc._original_arg)
+            g0new = replace(g0, {t: t + dt})
+            bcsnew.append(bc.reconstruct(V=V, g=g0new))
+        
+        return Fnew, bcsnew
+
+
+    def advance(self):
+
+        self.solver.solve(bounds=self.bounds)
+
+        # update previous steps
+        for i in range(len(self.us) - 1):
+            self.us[i].assign(self.us[i + 1])
+
+        # update solver statistics
+        self.num_steps += 1
+        self.num_nonlinear_iterations += self.solver.snes.getIterationNumber()
+        self.num_linear_iterations += self.solver.snes.getLinearSolveIterations()
+
+
+    # an optional method to mechanically find the required starting values via a single step method
+    def mechanized_startup(self, F, t, dt, u0, startup_params, bcs=None):
+        butcher_tableau = startup_params.get('tableau', None)
+        stepper_kwargs = startup_params.get('stepper_kwargs', {})
+        startup_dt_div = startup_params.get('dt_div', 1)
+        assert isinstance(startup_dt_div, int) and startup_dt_div > 0
+
+        self.us[0].assign(u0)
+        self.TS = TimeStepper(F, butcher_tableau, t, dt, u0, bcs=bcs, **stepper_kwargs)
+
+        
+        # modify the timestep
+        dt.assign(dt / startup_dt_div)
+        
+        # advance the system and assign values to previous steps
+        counter = 1
+        for i in range(self.s - 1):
+            for substep in range(startup_dt_div):
+                self.TS.advance()
+                t.assign(t + dt)
+            
+            if self.a[i+1] is not zero():
+                self.us[counter].assign(u0)
+                counter += 1
+
+        # reset the timestep
+        dt.assign(dt * startup_dt_div)
+
+
+    def solver_stats(self):
+
+        return (self.num_steps, self.num_nonlinear_iterations, self.num_linear_iterations)
+
+
+valid_multistep_kwargs = ("bounds", "startup_params")
+
+
+def MultistepTimeStepper(F, t, dt, u0, a, b, **kwargs):
+    base_kwargs = {}
+    for k in valid_base_kwargs:
+        if k in kwargs:
+            base_kwargs[k] = kwargs.pop(k)
+
+    bcs = kwargs.pop("bcs", None)
+    for cur_kwarg in kwargs.keys():
+        if cur_kwarg not in valid_multistep_kwargs:
+            raise ValueError(f"kwarg {cur_kwarg} is not allowable for MultistepTimeStepper")
+        
+    bounds = kwargs.pop('bounds', None)
+    startup_params = kwargs.pop('startup_params', {})
+    return MultistepStepper(F, t, dt, u0, a, b, bcs, startup_params=startup_params, bounds=bounds, **base_kwargs)
---
+++ tests/test_multistep.py
@@ -0,0 +1,271 @@
+import pytest
+from firedrake import (TestFunction, NonlinearVariationalProblem, NonlinearVariationalSolver,
+                       UnitSquareMesh, FunctionSpace, Function, grad, sin, pi, cos, project, 
+                       SpatialCoordinate, exp, inner, dx, div, norm, diff, DirichletBC)
+from irksome import (Dt, MeshConstant, TimeStepper, MultistepTimeStepper, RadauIIA, GaussLegendre)
+from ufl.algorithms import expand_derivatives
+import numpy as np
+
+lu_params = {
+    "snes_type": "ksponly",
+    "ksp_type": "preonly",
+    "mat_type": "aij",
+    "pc_type": "lu"
+}
+
+vi_params = {
+    "snes_type": "vinewtonrsls",
+    "snes_max_it": 300,
+    "snes_atol": 1.e-8,
+    "ksp_type": "preonly",
+    "pc_type": "lu",
+}
+
+
+def heat(msh, N, spatial_degree):
+
+    V = FunctionSpace(msh, "Bernstein", spatial_degree)
+
+    MC = MeshConstant(msh)
+    dt = MC.Constant(1 / N)
+    t = MC.Constant(0.0)
+
+    x, y = SpatialCoordinate(msh)
+    uexact = exp(-t) * cos(2 * pi * x) ** 2 * sin(2* pi * y) ** 2
+    rhs = expand_derivatives(diff(uexact, t)) - div(grad(uexact)) 
+
+    bc = DirichletBC(V, uexact, "on_boundary")
+
+    u0 = project(uexact, V, bcs=bc)
+    t.assign(float(t) + float(dt))
+    u1 = project(uexact, V, bcs=bc)
+
+    u2 = Function(V)
+    v = TestFunction(V)
+
+    F_hand = inner(u2, v) * dx - (4.0 / 3.0) * inner(u1, v) * dx + (1.0 / 3.0) * inner(u0, v) * dx - (2.0 / 3.0) * dt * (inner(rhs, v) * dx - inner(grad(u2), grad(v)) * dx)
+
+    stepper_prob = NonlinearVariationalProblem(F_hand, u2, bcs=bc)
+    stepper_solver = NonlinearVariationalSolver(stepper_prob, solver_parameters=lu_params)
+
+    for i in range(5):
+        t.assign(float(t) + float(dt))
+        stepper_solver.solve()
+        u0.assign(u1)
+        u1.assign(u2)
+        
+    return u2
+
+
+def heat_mech(msh, N, spatial_degree):
+
+    V = FunctionSpace(msh, "Bernstein", spatial_degree)
+
+    MC = MeshConstant(msh)
+    dt = MC.Constant(1 / N)
+    t = MC.Constant(0.0)
+
+    x, y = SpatialCoordinate(msh)
+    uexact = exp(-t) * cos(2 * pi * x) ** 2 * sin(2* pi * y) ** 2
+    rhs = expand_derivatives(diff(uexact, t)) - div(grad(uexact)) 
+
+    bc = DirichletBC(V, uexact, "on_boundary")
+
+    u0 = project(uexact, V, bcs=bc)
+    t.assign(float(t) + float(dt))
+    u1 = project(uexact, V, bcs=bc)
+
+    u = Function(V)
+    v = TestFunction(V)
+
+    F = inner(Dt(u), v) * dx - (inner(rhs, v) * dx - inner(grad(u), grad(v)) * dx)
+
+    ## BDF2
+    a = np.array([1.0 / 3.0, -4.0 / 3.0, 1.0])
+    b = np.array([0.0, 0.0, 2.0 / 3.0])
+
+    stepper = MultistepTimeStepper(F, t, dt, u, a, b, bcs=bc)
+    stepper.us[0].assign(u0)
+    stepper.us[1].assign(u1)
+
+    for i in range(5):
+        stepper.advance()
+        t.assign(float(t) + float(dt))
+
+    return u
+
+
+def heat_startup(msh, N, spatial_degree, startup_tableau, startup_dt_div):
+
+    V = FunctionSpace(msh, "Bernstein", spatial_degree)
+
+    MC = MeshConstant(msh)
+    dt = MC.Constant(1 / N)
+    t = MC.Constant(0.0)
+
+    x, y = SpatialCoordinate(msh)
+    uexact = exp(-t) * cos(2 * pi * x) ** 2 * sin(2* pi * y) ** 2
+    rhs = expand_derivatives(diff(uexact, t)) - div(grad(uexact)) 
+
+    bc = DirichletBC(V, uexact, "on_boundary")
+
+    u0 = project(uexact, V, bcs=bc)
+
+    u = project(uexact, V, bcs=bc)
+    v = TestFunction(V)
+
+    F = inner(Dt(u), v) * dx - (inner(rhs, v) * dx - inner(grad(u), grad(v)) * dx)
+
+    startup_stepper = TimeStepper(F, startup_tableau, t, dt, u, bcs=bc)
+
+
+    dt.assign(dt / startup_dt_div)
+    for i in range(0, startup_dt_div):
+        startup_stepper.advance()
+        t.assign(t + dt)
+    dt.assign(dt * startup_dt_div)
+    u1 = Function(V).assign(u)
+
+    ## BDF2
+    a = np.array([1.0 / 3.0, -4.0 / 3.0, 1.0])
+    b = np.array([0.0, 0.0, 2.0 / 3.0])
+
+    stepper = MultistepTimeStepper(F, t, dt, u, a, b, bcs=bc)
+    stepper.us[0].assign(u0)
+    stepper.us[1].assign(u1)
+
+    for i in range(5):
+        stepper.advance()
+        t.assign(float(t) + float(dt))
+    print(f'hand: {norm(u)}')
+    return u
+
+
+def heat_mech_startup(msh, N, spatial_degree, startup_tableau, startup_dt_div):
+
+    V = FunctionSpace(msh, "Bernstein", spatial_degree)
+
+    MC = MeshConstant(msh)
+    dt = MC.Constant(1 / N)
+    t = MC.Constant(0.0)
+
+    x, y = SpatialCoordinate(msh)
+    uexact = exp(-t) * cos(2 * pi * x) ** 2 * sin(2* pi * y) ** 2
+    rhs = expand_derivatives(diff(uexact, t)) - div(grad(uexact)) 
+
+    bc = DirichletBC(V, uexact, "on_boundary")
+
+    u = project(uexact, V, bcs=bc)
+    v = TestFunction(V)
+
+    F = inner(Dt(u), v) * dx - (inner(rhs, v) * dx - inner(grad(u), grad(v)) * dx)
+
+    ## BDF2
+    a = np.array([1.0 / 3.0, -4.0 / 3.0, 1.0])
+    b = np.array([0.0, 0.0, 2.0 / 3.0])
+
+    startup_params = {'tableau': startup_tableau,
+                      'dt_div': startup_dt_div
+                     }
+
+    t.assign(0.0)
+    stepper = MultistepTimeStepper(F, t, dt, u, a, b, bcs=bc, startup_params=startup_params)
+
+    for i in range(5):
+        stepper.advance()
+        t.assign(float(t) + float(dt))
+    print(f'mech: {norm(u)}')
+    return u
+
+def heat_bounds(bounds_flag, startup_bounds_flag, startup_tableau):
+    N = 16
+    msh = UnitSquareMesh(N, N)
+
+    V = FunctionSpace(msh, "Bernstein", 2)
+
+    MC = MeshConstant(msh)
+    dt = MC.Constant(1 / N)
+    t = MC.Constant(0.0)
+
+    x, y = SpatialCoordinate(msh)
+    uexact = exp(-t) * cos(2 * pi * x) ** 2 * sin(2* pi * y) ** 2
+    rhs = expand_derivatives(diff(uexact, t)) - div(grad(uexact)) 
+
+    bc = DirichletBC(V, uexact, "on_boundary")
+
+    lower = Function(V).assign(0.0)
+    upper = Function(V).assign(np.inf)
+
+    u = Function(V)
+    v = TestFunction(V)
+
+    F_proj = inner(u - uexact, v) * dx
+    project_IC_prob = NonlinearVariationalProblem(F_proj, u, bcs=bc)
+    project_IC = NonlinearVariationalSolver(project_IC_prob, solver_parameters=vi_params)
+    project_IC.solve(bounds=(lower, upper))
+
+    F = inner(Dt(u), v) * dx - (inner(rhs, v) * dx - inner(grad(u), grad(v)) * dx)
+
+    ## BDF2
+    a = np.array([1.0 / 3.0, -4.0 / 3.0, 1.0])
+    b = np.array([0.0, 0.0, 2.0 / 3.0])
+
+    t.assign(0.0)
+
+    if startup_bounds_flag:
+        startup_bounds = ('stage', lower, upper)
+    else:
+        startup_bounds = None
+
+    stepper_kwargs = {'solver_parameters': vi_params,
+                      'stage_type': 'value',
+                      'basis_type': 'Bernstein', 
+                      'bounds': startup_bounds 
+                      }
+
+    startup_params = {'tableau': startup_tableau,
+                      'dt_div': 2,
+                      'stepper_kwargs': stepper_kwargs
+                     }
+    
+    if bounds_flag:
+        bounds = (lower, upper)
+    else:
+        bounds = None
+
+    stepper = MultistepTimeStepper(F, t, dt, u, a, b, bcs=bc, bounds=bounds, solver_parameters=vi_params, startup_params=startup_params)
+
+    min_init = min(stepper.us[0].dat.data)
+    min_step1 = min(stepper.us[1].dat.data)
+    stepper.advance()
+    t.assign(t + dt)
+    min_step2 = min(u.dat.data)
+
+    return (min_init >= 0.0, min_step1 >= 0.0, min_step2 >= 0)
+
+@pytest.mark.parametrize('N', [8, 16, 32])
+@pytest.mark.parametrize('spatial_degree', [1, 2, 3])
+def test_heat_mech(N, spatial_degree):
+    msh = UnitSquareMesh(N, N)
+    u1 = heat(msh, N, spatial_degree)
+    u2 = heat_mech(msh, N, spatial_degree)
+    assert norm(u1 - u2) / norm(u1) < 1e-13
+
+
+@pytest.mark.parametrize('N', [16, 32])
+@pytest.mark.parametrize('spatial_degree', [1, 2])
+@pytest.mark.parametrize('startup_tableau', [RadauIIA(1), RadauIIA(2), GaussLegendre(2)])
+@pytest.mark.parametrize('startup_dt_div', [1, 8])
+def test_heat_startup(N, spatial_degree, startup_tableau, startup_dt_div):
+    msh = UnitSquareMesh(N, N)
+    u1 = heat_startup(msh, N, spatial_degree, startup_tableau, startup_dt_div)
+    u2 = heat_mech_startup(msh, N, spatial_degree, startup_tableau, startup_dt_div)
+    assert norm(u1 - u2) / norm(u1) < 1e-13
+
+
+@pytest.mark.parametrize('bounds_flag', (True, False))
+@pytest.mark.parametrize('startup_bounds_flag', (True, False))
+@pytest.mark.parametrize('startup_tableau', (RadauIIA(1), RadauIIA(2), GaussLegendre(1)))
+def test_heat_bounds(bounds_flag, startup_bounds_flag, startup_tableau):
+    tup = heat_bounds(bounds_flag, startup_bounds_flag, startup_tableau)
+    assert tup == (True, startup_bounds_flag, bounds_flag)
