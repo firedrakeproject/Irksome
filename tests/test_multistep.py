01451ec9fdc5289c52c420f7593e4e8100db4103
John Stephens <john_stephens2@baylor.edu>
2025-11-14 10:21:38 -0600

    remove duplicate tests

--- tests/test_multistep.py
+++ tests/test_multistep.py
@@ -96,88 +96,6 @@ def heat_mech(msh, N, spatial_degree):
     return u
 
 
-# def heat_startup(msh, N, spatial_degree, startup_tableau, startup_dt_div):
-
-#     V = FunctionSpace(msh, "Bernstein", spatial_degree)
-
-#     MC = MeshConstant(msh)
-#     dt = MC.Constant(1 / N)
-#     t = MC.Constant(0.0)
-
-#     x, y = SpatialCoordinate(msh)
-#     uexact = exp(-t) * cos(2 * pi * x) ** 2 * sin(2* pi * y) ** 2
-#     rhs = expand_derivatives(diff(uexact, t)) - div(grad(uexact)) 
-
-#     bc = DirichletBC(V, uexact, "on_boundary")
-
-#     u0 = project(uexact, V, bcs=bc)
-
-#     u = project(uexact, V, bcs=bc)
-#     v = TestFunction(V)
-
-#     F = inner(Dt(u), v) * dx - (inner(rhs, v) * dx - inner(grad(u), grad(v)) * dx)
-
-#     startup_stepper = TimeStepper(F, startup_tableau, t, dt, u, bcs=bc)
-
-
-#     dt.assign(dt / startup_dt_div)
-#     for i in range(0, startup_dt_div):
-#         startup_stepper.advance()
-#         t.assign(t + dt)
-#     dt.assign(dt * startup_dt_div)
-#     u1 = Function(V).assign(u)
-
-#     ## BDF2
-#     a = np.array([1.0 / 3.0, -4.0 / 3.0, 1.0])
-#     b = np.array([0.0, 0.0, 2.0 / 3.0])
-
-#     stepper = MultistepTimeStepper(F, t, dt, u, (a, b), bcs=bc)
-#     stepper.us[0].assign(u0)
-#     stepper.us[1].assign(u1)
-
-#     for i in range(5):
-#         stepper.advance()
-#         t.assign(float(t) + float(dt))
-#     print(f'hand: {norm(u)}')
-#     return u
-
-
-# def heat_mech_startup(msh, N, spatial_degree, startup_tableau, startup_dt_div):
-
-#     V = FunctionSpace(msh, "Bernstein", spatial_degree)
-
-#     MC = MeshConstant(msh)
-#     dt = MC.Constant(1 / N)
-#     t = MC.Constant(0.0)
-
-#     x, y = SpatialCoordinate(msh)
-#     uexact = exp(-t) * cos(2 * pi * x) ** 2 * sin(2* pi * y) ** 2
-#     rhs = expand_derivatives(diff(uexact, t)) - div(grad(uexact)) 
-
-#     bc = DirichletBC(V, uexact, "on_boundary")
-
-#     u = project(uexact, V, bcs=bc)
-#     v = TestFunction(V)
-
-#     F = inner(Dt(u), v) * dx - (inner(rhs, v) * dx - inner(grad(u), grad(v)) * dx)
-
-#     ## BDF2
-#     a = np.array([1.0 / 3.0, -4.0 / 3.0, 1.0])
-#     b = np.array([0.0, 0.0, 2.0 / 3.0])
-
-#     startup_params = {'tableau': startup_tableau,
-#                       'dt_div': startup_dt_div
-#                      }
-
-#     t.assign(0.0)
-#     stepper = MultistepTimeStepper(F, t, dt, u, (a, b), bcs=bc, startup_params=startup_params)
-
-#     for i in range(5):
-#         stepper.advance()
-#         t.assign(float(t) + float(dt))
-#     print(f'mech: {norm(u)}')
-#     return u
-
 def heat_bounds(bounds_flag, startup_bounds_flag, startup_tableau):
     N = 16
     msh = UnitSquareMesh(N, N)
@@ -357,17 +275,6 @@ def test_heat_mech(N, spatial_degree):
     u1 = heat(msh, N, spatial_degree)
     u2 = heat_mech(msh, N, spatial_degree)
     assert norm(u1 - u2) / norm(u1) < 1e-13
-
-
-# @pytest.mark.parametrize('N', [16, 32])
-# @pytest.mark.parametrize('spatial_degree', [1, 2])
-# @pytest.mark.parametrize('startup_tableau', [RadauIIA(1), RadauIIA(2), GaussLegendre(2)])
-# @pytest.mark.parametrize('startup_dt_div', [1, 8])
-# def test_heat_startup(N, spatial_degree, startup_tableau, startup_dt_div):
-#     msh = UnitSquareMesh(N, N)
-#     u1 = heat_startup(msh, N, spatial_degree, startup_tableau, startup_dt_div)
-#     u2 = heat_mech_startup(msh, N, spatial_degree, startup_tableau, startup_dt_div)
-#     assert norm(u1 - u2) / norm(u1) < 1e-13
 
 
 @pytest.mark.parametrize('bounds_flag', (True, False))
