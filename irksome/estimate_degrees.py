80567c2fd46ea998d0405ea125a05b2dba9ecebd
Pablo Brubeck <brubeck@protonmail.com>
2025-12-03 11:07:28 +0000

    TimeDegreeEstimator

---
+++ irksome/estimate_degrees.py
@@ -0,0 +1,166 @@
+from functools import singledispatchmethod
+
+from ufl.corealg.dag_traverser import DAGTraverser
+
+from ufl.constantvalue import IntValue
+from ufl.classes import (
+    Argument, ConstantValue, Coefficient, SpatialCoordinate,
+    Abs, Conj, Curl, Derivative, Div, Grad, Indexed, ReferenceGrad,
+    ReferenceValue, Variable, ComponentTensor, IndexSum, Skew, Sym, Trace,
+    Transposed, Determinant, Inverse, Division, Product, Inner, Dot, Outer,
+    Cross, Sum, ListTensor, ExprList, ExprMapping, Power, MathFunction,
+    Conditional, Condition, MultiIndex, MaxValue, MinValue, Form, Integral,
+)
+
+from .deriv import TimeDerivative
+
+
+class TimeDegreeEstimator(DAGTraverser):
+    """Time degree estimator.
+
+    This algorithm is exact for a few operators and heuristic for many.
+    """
+    def __init__(self, test_degree, trial_degree, t=None, timedep_coeffs=None, **kwargs):
+        super().__init__(**kwargs)
+        self.test_degree = test_degree
+        self.trial_degree = trial_degree
+        self.t = t
+        self.timedep_coeffs = timedep_coeffs
+
+    # Work around singledispatchmethod inheritance issue;
+    # see https://bugs.python.org/issue36457.
+    @singledispatchmethod
+    def process(self, o):
+        return super().process(o)
+
+    @process.register(Argument)
+    def argument(self, o):
+        return self.test_degree if o.number() == 0 else self.trial_degree
+
+    @process.register(ConstantValue)
+    def constant(self, o):
+        if self.t is not None and o is self.t:
+            return 1
+        else:
+            return 0
+
+    @process.register(Coefficient)
+    @process.register(SpatialCoordinate)
+    def terminal(self, o):
+        if self.t is not None and o is self.t:
+            return 1
+        elif self.timedep_coeffs is None or o in self.timedep_coeffs:
+            return self.trial_degree
+        else:
+            return 0
+
+    @process.register(TimeDerivative)
+    @DAGTraverser.postorder
+    def time_derivative(self, o, degree):
+        return degree - 1
+
+    @process.register(Abs)
+    @process.register(Conj)
+    @process.register(Curl)
+    @process.register(Derivative)
+    @process.register(Div)
+    @process.register(Grad)
+    @process.register(Indexed)
+    @process.register(ReferenceGrad)
+    @process.register(ReferenceValue)
+    @process.register(Variable)
+    @process.register(ComponentTensor)
+    @process.register(IndexSum)
+    @process.register(Skew)
+    @process.register(Sym)
+    @process.register(Trace)
+    @process.register(Transposed)
+    @DAGTraverser.postorder
+    def terminal_modifier(self, o, degree, *ops):
+        return degree
+
+    @process.register(Determinant)
+    @process.register(Inverse)
+    @DAGTraverser.postorder
+    def not_handled(self, v, *ops):
+        raise NotImplementedError(f"Degree estimation for {type(v).__name__} not handled.")
+
+    @process.register(Division)
+    @process.register(Product)
+    @process.register(Inner)
+    @process.register(Dot)
+    @process.register(Outer)
+    @process.register(Cross)
+    @DAGTraverser.postorder
+    def add_degrees(self, v, *ops):
+        return sum(ops)
+
+    @process.register(Sum)
+    @process.register(ListTensor)
+    @process.register(ExprList)
+    @process.register(ExprMapping)
+    @DAGTraverser.postorder
+    def max_degree(self, v, *ops):
+        return max(ops)
+
+    @process.register(Power)
+    @DAGTraverser.postorder
+    def power(self, v, a, b):
+        """Apply to power.
+
+        If b is a positive integer: degree(a**b) == degree(a)*b
+        otherwise use the heuristic: degree(a**b) == degree(a) + 2.
+        """
+        _f, g = v.ufl_operands
+        if isinstance(g, IntValue):
+            gi = g.value()
+            if gi >= 0:
+                return a * gi
+        # Something to a non-(positive integer) power, e.g. float,
+        # negative integer, Coefficient, etc.
+        return a + 2
+
+    @process.register(MathFunction)
+    @DAGTraverser.postorder
+    def math_function(self, v, a):
+        """Apply to math_function.
+
+        Using the heuristic:
+        degree(sin(const)) == 0
+        degree(sin(a)) == degree(a)+2
+        which can be wildly inaccurate but at least gives a somewhat
+        high integration degree.
+        """
+        if a:
+            return a + 2
+        else:
+            return a
+
+    @process.register(Conditional)
+    @DAGTraverser.postorder
+    def conditional(self, v, c, *ops):
+        return max(*ops)
+
+    @process.register(MinValue)
+    @process.register(MaxValue)
+    @DAGTraverser.postorder
+    def minmax(self, v, *ops):
+        return max(*ops)
+
+    @process.register(Condition)
+    @process.register(MultiIndex)
+    @DAGTraverser.postorder
+    def non_numeric(self, v, *args):
+        return None
+
+
+def estimate_time_degree(expression, test_degree, trial_degree, t=None, timedep_coeffs=None):
+    de = TimeDegreeEstimator(test_degree, trial_degree, t=t, timedep_coeffs=timedep_coeffs)
+    if isinstance(expression, Form):
+        if not expression.integrals():
+            return 0
+        return max(map(de, (it.integrand() for it in expression.integrals())))
+    elif isinstance(expression, Integral):
+        return de(expression.integrand())
+    else:
+        return de(expression)
---
+++ tests/test_degree_estimation.py
@@ -0,0 +1,111 @@
+import pytest
+from irksome import Dt
+from firedrake import *
+
+from irksome.estimate_degrees import estimate_time_degree
+
+
+@pytest.fixture
+def mesh():
+    return UnitSquareMesh(1, 1)
+
+
+@pytest.fixture(params=("scalar", "vector"))
+def V(request, mesh):
+    if request.param == "scalar":
+        return FunctionSpace(mesh, "CG", 1)
+    elif request.param == "vector":
+        return VectorFunctionSpace(mesh, "CG", 1)
+
+
+def test_time():
+    k = 3
+
+    t = Constant(0)
+    expr = t
+    expected = 1
+    assert estimate_time_degree(expr, k-1, k, t=t, timedep_coeffs=()) == expected
+
+    expr = (t-1)**2
+    expected = 2
+    assert estimate_time_degree(expr, k-1, k, t=t, timedep_coeffs=()) == expected
+
+
+@pytest.mark.parametrize("order", (1, 2, 3))
+def test_time_derivative(V, order):
+    k = 3
+    u = Function(V)
+    expr = Dt(u, order)
+    expected = k - order
+    time_degree = estimate_time_degree(expr, k-1, k, timedep_coeffs=(u,))
+    assert time_degree == expected
+
+
+def test_grad(V):
+    k = 3
+    u = Function(V)
+    expr = grad(u)
+
+    expected = k
+    time_degree = estimate_time_degree(expr, k-1, k, timedep_coeffs=(u,))
+    assert time_degree == expected
+
+
+def test_form(V):
+    k = 3
+    u = Function(V)
+    expr = inner(grad(u), grad(u)) * dx
+
+    expected = 2 * k
+    time_degree = estimate_time_degree(expr, k-1, k, timedep_coeffs=(u,))
+    assert time_degree == expected
+
+
+def test_form_Dt(V):
+    k = 3
+    trial_degree = k
+    test_degree = k - 1
+
+    u = Function(V)
+    v = TestFunction(V)
+    expr = inner(Dt(u), v) * dx
+
+    expected = trial_degree-1 + test_degree
+    time_degree = estimate_time_degree(expr, test_degree, trial_degree, timedep_coeffs=(u,))
+    assert time_degree == expected
+
+
+def test_nonlinear_form(V):
+    k = 3
+    trial_degree = k
+    test_degree = k - 1
+
+    u = Function(V)
+    v = TestFunction(V)
+    if u.ufl_shape == ():
+        dim, = grad(u).ufl_shape
+        c = as_vector([u] + [0]*(dim-1))
+    else:
+        c = u
+    expr = inner(dot(grad(u), c), v) * dx + inner(Dt(u), v) * dx
+
+    expected = 2 * trial_degree + test_degree
+    time_degree = estimate_time_degree(expr, test_degree, trial_degree, timedep_coeffs=(u,))
+    assert time_degree == expected
+
+
+def test_mixed_form(V):
+    k = 3
+    trial_degree = k
+    test_degree = k - 1
+
+    Z = V * V
+    z = Function(Z)
+
+    v0, v1 = TestFunctions(Z)
+    u0, u1 = split(z)
+    expr = inner(Dt(u0), v0) * dx + inner(Dt(u1), v1) * dx
+
+    expected = trial_degree-1 + test_degree
+    time_degree = estimate_time_degree(expr, test_degree, trial_degree, timedep_coeffs=(z,))
+    assert time_degree == expected
