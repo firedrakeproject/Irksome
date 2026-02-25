5def49936fa6ca0bee3bf0c569173188a61498c7
Jørgen S. Dokken <dokken@simula.no>
2026-02-25 13:59:28 +0100

    Make documentation run without DOLFINx installed

--- irksome/backends/dolfinx.py
+++ irksome/backends/dolfinx.py
@@ -1,49 +1,49 @@
 """DOLFINx backend for Irksome"""
 
-import basix.ufl
-import ufl
-import dolfinx
+try:
+    import basix.ufl
+    import dolfinx
+    import ufl
 
+    def get_function_space(u: ufl.Coefficient) -> ufl.FunctionSpace:
+        return u.ufl_function_space()
 
-def get_function_space(u: ufl.Coefficient) -> ufl.FunctionSpace:
-    return u.ufl_function_space()
+    def get_stages(V: dolfinx.fem.FunctionSpace, num_stages: int) -> ufl.Coefficient:
+        """
+        Given a function space for a single time-step, get a duplicate of this space,
+        repeated `num_stages` times.
 
+        Args:
+            V: Space for single step
+            num_stages: Number of stages
 
-def get_stages(V: dolfinx.fem.FunctionSpace, num_stages: int) -> ufl.Coefficient:
-    """
-    Given a function space for a single time-step, get a duplicate of this space,
-    repeated `num_stages` times.
+        Returns:
+            A coefficient in the new function space
+        """
+        if V.num_sub_spaces == 0:
+            el = basix.ufl.mixed_element([V.ufl_element()] * num_stages)
+        else:
+            el = basix.ufl.mixed_element(V.ufl_element().sub_elements * num_stages)
+        Vbig = dolfinx.fem.functionspace(V.mesh, el)
+        return dolfinx.fem.Function(Vbig)
 
-    Args:
-        V: Space for single step
-        num_stages: Number of stages
+    class MeshConstant(object):
+        def __init__(self, msh):
+            self.msh = msh
+            try:
+                import scifem
+            except ModuleNotFoundError:
+                raise RuntimeError("Scifem is required to make mesh-constants")
 
-    Returns:
-        A coefficient in the new function space
-    """
-    if V.num_sub_spaces == 0:
-        el = basix.ufl.mixed_element([V.ufl_element()] * num_stages)
-    else:
-        el = basix.ufl.mixed_element(V.ufl_element().sub_elements * num_stages)
-    Vbig = dolfinx.fem.functionspace(V.mesh, el)
-    return dolfinx.fem.Function(Vbig)
+            self.V = scifem.create_real_functionspace(msh, ())
 
+        def Constant(self, val=0.0) -> ufl.Coefficient:
+            v = dolfinx.fem.Function(self.V)
+            v.value = val
+            return v
 
-class MeshConstant(object):
-    def __init__(self, msh):
-        self.msh = msh
-        try:
-            import scifem
-        except ModuleNotFoundError:
-            raise RuntimeError("Scifem is required to make mesh-constants")
+    def get_mesh_constant(MC: MeshConstant | None) -> ufl.core.expr.Expr:
+        return MC.Constant if MC is not None else ufl.constantvalue.ComplexValue
 
-        self.V = scifem.create_real_functionspace(msh, ())
-
-    def Constant(self, val=0.0) -> ufl.Coefficient:
-        v = dolfinx.fem.Function(self.V)
-        v.value = val
-        return v
-
-
-def get_mesh_constant(MC: MeshConstant | None) -> ufl.core.expr.Expr:
-    return MC.Constant if MC is not None else ufl.constantvalue.ComplexValue
+except ModuleNotFoundError:
+    pass
