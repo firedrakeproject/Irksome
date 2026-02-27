591aca54ae082b2c73cc71d0ea91804d88d855b3
Daniel Shapero <shapero.daniel@gmail.com>
2026-02-27 10:24:32 -0800

    Add test for stage value stepper initialization

---
+++ tests/test_stage_value_init.py
@@ -0,0 +1,50 @@
+from firedrake import *
+from irksome import Dt, TimeStepper, BackwardEuler
+
+
+def test_stage_value_init():
+    nx = 16
+    lx = 1.0
+    mesh = IntervalMesh(nx, lx)
+
+    Q = FunctionSpace(mesh, "DG", 0)
+    x, = SpatialCoordinate(mesh)
+
+    p = Function(Q)
+    p_in = Constant(0.5)
+    p.assign(p_in)
+
+    q = TestFunction(Q)
+
+    u_0 = Constant(1.0)
+    δu = Constant(1.0)
+    Lx = Constant(lx)
+    u = as_vector((u_0 + δu * x / Lx,))
+
+    p_max = Constant(2.0)
+    s = p_max / p - 1
+    F_cells = (Dt(p) * q - inner(p * u, grad(q)) - s * q) * dx
+    n = FacetNormal(mesh)
+    f_n = p * max_value(0, inner(u, n))
+    F_facets = (f_n("+") - f_n("-")) * (q("+") - q("-")) * dS
+    F_inflow = p_in * min_value(0, inner(u, n)) * q * ds
+    F_outflow = p * max_value(0, inner(u, n)) * q * ds
+    F = F_cells + F_facets + F_inflow + F_outflow
+
+    method = BackwardEuler()
+    t = Constant(0.0)
+    timestep = 0.5 / nx
+    dt = Constant(timestep)
+    params = {
+        "stage_type": "value",
+        "basis_type": "Bernstein",
+        "solver_parameters": {"snes_monitor": None},
+    }
+    stepper = TimeStepper(F, method, t, dt, p, **params)
+
+    final_time = 2.0
+    num_steps = int(final_time / timestep)
+    for step in range(num_steps):
+        stepper.advance()
+
+    assert norm(p) > 0.0
