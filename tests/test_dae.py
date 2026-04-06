737bbd0f03f92a2d7fbab4730f553cfc1d50af2d
Scott MacLachlan <smaclachlan@mun.ca>
2026-04-06 16:38:44 -0230

    Removing mass solve from stage_type value

--- tests/test_dae.py
+++ tests/test_dae.py
@@ -1,8 +1,10 @@
-from firedrake import (Function, FunctionSpace, TestFunction, TestFunctions,
-                       UnitTriangleMesh, VectorFunctionSpace, div, dx, grad,
-                       inner, split)
-from irksome import Dt
+from firedrake import (Constant, DirichletBC, FacetNormal, Function, FunctionSpace, SpatialCoordinate, TestFunction, TestFunctions,
+                       UnitTriangleMesh, UnitSquareMesh, VectorFunctionSpace, as_vector, div, dot, ds, dx, errornorm, grad,
+                       inner, project, split)
+from irksome import Dt, GaussLegendre, RadauIIA, TimeStepper
 from irksome.tools import is_ode
+import pytest
+import numpy as np
 
 
 # tests the is_ode tool to confirm it's determined whether something is an
@@ -42,3 +44,49 @@ def test_foo():
 
     assert not is_ode(F, up)
     assert is_ode(F + inner(Dt(p), w) * dx, up)
+
+
+@pytest.mark.parametrize('tableau', [RadauIIA, GaussLegendre])
+@pytest.mark.parametrize('temporal_degree', [2, 3])
+@pytest.mark.parametrize('stage_type', ["deriv", "value"])
+def test_mixed_heat(tableau, temporal_degree, stage_type):
+
+    msh = UnitSquareMesh(4, 4)
+    V = FunctionSpace(msh, "RT", 3)
+    W = FunctionSpace(msh, "DG", 2)
+    Z = V * W
+
+    butcher_tableau = tableau(temporal_degree)
+
+    dt = Constant(0.05)
+    t = Constant(0.0)
+
+    x, y = SpatialCoordinate(msh)
+
+    u_exact = (x*(1-x)+y*(1-y))*(1+t)
+    q_exact = -grad(u_exact)
+    qu_exact = as_vector([q_exact[0], q_exact[1], u_exact])
+    f = Dt(u_exact) + div(q_exact)
+
+    qu = project(qu_exact, Z)
+    q, u = split(qu)
+
+    v, w = TestFunctions(Z)
+    n = FacetNormal(msh)
+
+    F = (inner(Dt(u), w) * dx + inner(div(q), w) * dx - inner(f, w) * dx
+         + inner(q, v) * dx - inner(u, div(v)) * dx - inner(u_exact, dot(v, n))*ds)
+
+    bc = DirichletBC(Z.sub(0), q_exact, "on_boundary")
+
+    stepper = TimeStepper(F, butcher_tableau, t, dt, qu, bcs=bc,
+                          stage_type=stage_type)
+
+    e_vals = []
+    for i in range(2):
+        stepper.advance()
+
+        t.assign(float(t)+float(dt))
+        e_vals.append(errornorm(qu_exact, qu))
+
+    assert np.allclose(e_vals, 0)
