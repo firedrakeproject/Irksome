f13381fb2a0ab9622533dccc366ec3f5c8c7b606
Robert Kirby <robert.c.kirby@gmail.com>
2026-04-21 13:47:49 -0500

    Update irksome/imex.py
    Co-authored-by: ScottMacLachlan <smaclachlan@mun.ca>

--- irksome/imex.py
+++ irksome/imex.py
@@ -635,3 +635,4 @@ class DIRKIMEXMethod:
         Forces the matrix to be reassembled next time it is required.
         """
         LinearVariationalSolver.invalidate_jacobian(self.solver)
+        LinearVariationalSolver.invalidate_jacobian(self.mass_solver)
