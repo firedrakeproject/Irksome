994168c92ed673e6669de92553dc5ed66eabeb05
Daniel Shapero <shapero.daniel@gmail.com>
2026-05-02 06:09:30 -0700

    Stub out test for Stefan problem

---
+++ tests/test_lag.py
@@ -0,0 +1,40 @@
+import pytest
+import firedrake
+from firedrake import Constant, inner, grad, dx, conditional
+import irksome
+from irksome import Dt
+
+
+def test_stefan_implicit():
+    """Test lagging the conductivity on the Stefan problem"""
+    nx = 32
+    mesh = firedrake.UnitIntervalMesh(nx)
+    V = firedrake.FunctionSpace(mesh, "CG", 1)
+    x, = firedrake.SpatialCoordinate(mesh)
+
+    u = firedrake.Function(V)
+    u.interpolate(1 - 2 * x)
+
+    k_solid = Constant(2.0)
+    k_liquid = Constant(1.0)
+    k = conditional(u < 0, k_solid, k_liquid)
+
+    v = firedrake.TestFunction(V)
+    F = (Dt(u) * v + k * inner(grad(u), grad(v))) * dx
+
+    T_1 = Constant(1.0)
+    T_2 = Constant(-1.0)
+    bcs = [firedrake.DirichletBC(V, T_1, 1), firedrake.DirichletBC(V, T_2, 2)]
+
+    t = Constant(0.0)
+    dt = Constant(0.1)
+    solver_params = {"snes_type": "newtonls", "snes_converged_reason": None}
+    params = {"bcs": bcs, "solver_parameters": solver_params}
+    method = irksome.BackwardEuler()
+    stepper = irksome.TimeStepper(F, method, t, dt, u, **params)
+
+    with pytest.raises(firedrake.ConvergenceError):
+        final_time = 10.0
+        num_steps = int(final_time / float(dt))
+        for step in range(num_steps):
+            stepper.advance()
