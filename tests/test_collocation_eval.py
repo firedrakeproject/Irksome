752351e52870990d13fd10cc25de99fe0262f17a
Pablo Brubeck <brubeck@protonmail.com>
2026-05-18 11:31:54 +0100

    Apply suggestion from @pbrubeck

--- tests/test_collocation_eval.py
+++ tests/test_collocation_eval.py
@@ -7,7 +7,7 @@ from firedrake import (Constant, TestFunction, UnitSquareMesh, FunctionSpace, Fu
 from irksome import (GaussLegendre, RadauIIA, Dt, TimeStepper,
                      ContinuousPetrovGalerkinScheme, DiscontinuousGalerkinScheme)
 from irksome.tableaux.ButcherTableaux import ButcherTableau
-from irksome.backends.firedrake import replace
+from irksome.tools import replace
 from FIAT import ufc_simplex
 from FIAT.barycentric_interpolation import LagrangePolynomialSet
 from FIAT.bernstein import Bernstein
