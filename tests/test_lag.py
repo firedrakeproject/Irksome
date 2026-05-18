3048a2b556713b53728c50ddd5fde1a1c8c21f39
Daniel Shapero <shapero.daniel@gmail.com>
2026-05-18 14:27:54 -0700

    Add asserts + check implicit form fails in lag test

--- tests/test_lag.py
+++ tests/test_lag.py
@@ -1,5 +1,6 @@
+import pytest
 import firedrake
-from firedrake import Constant, inner, grad, dx, conditional
+from firedrake import Constant, inner, grad, dx, conditional, assemble, replace
 import irksome
 from irksome import Dt, lag
 
@@ -13,26 +14,43 @@ def test_stefan_implicit():
 
     u = firedrake.Function(V)
     u.interpolate(1 - 2 * x)
-
-    k_solid = Constant(2.0)
-    k_liquid = Constant(1.0)
-    k = lag(conditional(u < 0, k_solid, k_liquid))
-
-    v = firedrake.TestFunction(V)
-    F = (Dt(u) * v + k * inner(grad(u), grad(v))) * dx
+    u_0 = u.copy(deepcopy=True)
 
     T_1 = Constant(1.0)
     T_2 = Constant(-1.0)
     bcs = [firedrake.DirichletBC(V, T_1, 1), firedrake.DirichletBC(V, T_2, 2)]
 
+    v = firedrake.TestFunction(V)
     t = Constant(0.0)
     dt = Constant(0.1)
+    final_time = 10.0
+    num_steps = int(final_time / float(dt))
     solver_params = {"snes_type": "newtonls", "snes_converged_reason": None}
     params = {"bcs": bcs, "solver_parameters": solver_params}
     method = irksome.BackwardEuler()
+
+    k_solid = Constant(2.0)
+    k_liquid = Constant(1.0)
+
+    # Check that fully implicit form fails
+    k = conditional(u < 0, k_solid, k_liquid)
+    F = (Dt(u) * v + k * inner(grad(u), grad(v))) * dx
+    stepper = irksome.TimeStepper(F, method, t, dt, u, **params)
+    with pytest.raises(firedrake.ConvergenceError):
+        for step in range(num_steps):
+            stepper.advance()
+
+    # Check that the lagged form works
+    u.assign(u_0)
+    F = (Dt(u) * v + lag(k) * inner(grad(u), grad(v))) * dx
     stepper = irksome.TimeStepper(F, method, t, dt, u, **params)
 
-    final_time = 10.0
-    num_steps = int(final_time / float(dt))
     for step in range(num_steps):
         stepper.advance()
+
+    energy = 0.5 * u**2 * dx
+    initial_energy = assemble(replace(energy, {u: u_0}))
+    final_energy = assemble(energy)
+    print(f"Initial energy: {initial_energy}")
+    print(f"Final:          {final_energy}")
+    assert final_energy <= initial_energy
